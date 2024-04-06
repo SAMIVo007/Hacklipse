1d36a9a9f1ceb41a46b384944ee61078f3ed83dc
Rahul Goyal <87524423+Goyalrahul1516@users.noreply.github.com>
2024-04-07 05:29:44 +0530

    added create provider

--- source/tables/createprovider.js
+++ source/tables/createprovider.js
@@ -1,12 +1,14 @@
 import { collection, addDoc } from "firebase/firestore"
 import { db } from "../config/firebase"
 
-export function CreateUser(username,phone,email,password) {
-    const userDb = collection(db, "users")
+export function CreateUser(username,phone,email,password,speciality,fee) {
+    const userDb = collection(db, "providers")
     addDoc(userDb, {
       username: username,
       phone: phone,
       email: email,
       password: password,
+      speciality: speciality,
+      fee: fee,
     })
 }
